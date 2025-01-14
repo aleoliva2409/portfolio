046a7350128269c875723e141903df3558898fd5
Alejandro Oliva <alejandro.oliva2409@gmail.com>
2025-01-14 02:04:17 -0300

    header component and many configs added

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,17 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      width: {
+        wrapper: '900px',
+      },
+      fontFamily: {
+        sans: ['Onest Variable', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
