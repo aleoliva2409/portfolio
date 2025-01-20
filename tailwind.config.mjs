9e86cdaf8be103b5e25068ea697742c87f2f24c1
Alejandro Oliva <alejandro.oliva2409@gmail.com>
2025-01-20 19:58:16 -0300

    few changes added

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,7 +6,7 @@ export default {
   theme: {
     extend: {
       width: {
-        wrapper: '900px',
+        wrapper: '53.125rem',
       },
       fontFamily: {
         sans: ['Onest Variable', ...defaultTheme.fontFamily.sans],
